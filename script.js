bf205125b418937b8c4d8bfaf71c5c9eefe9ca24
frade10 <rodrigopfrade@icloud.com>
2026-01-15 09:30:35 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+
+/* Botao Dark Mode  */
 const btn = document.getElementById("darkModeBtn");
 
 btn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
+});
+
+
+/* Botao de voltar ao topo  */
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+});
+
+backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 });
